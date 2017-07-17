383edafb1d31ffd05dc30c438805d9c110109439
Colby Meier <colbymeier1337@gmail.com>
2017-07-16 19:52:42 -0600

    tailrec

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,7 @@ object Main {
   val precisionIntent = 100000 // How many simulations to run for a single generation: Higher == Longer per Generation
   val generationCount = 1000 // How many generations to run for each simulation: Higher == Longer per Generation
   val totalGenerations = 10000 // How many generations to average: Higher == More Iterations
-//
+
   def main(args: Array[String]):Unit = {
     val generations = for {
       _ <- Range(0, totalGenerations)
@@ -13,7 +13,7 @@ object Main {
     } yield generation
     println(s"Final Guess After ${totalGenerations} Sequences: ${generations.sum / generations.size}")
   }
-//
+
   def generationValue(simulationPrecision: Int, generationCount: Int): IndexedSeq[Double] = {
     val gen = for {
       _ <- Range(0, generationCount)
@@ -22,12 +22,12 @@ object Main {
     println(s"Generation Guess: ${gen.sum / gen.size}")
     gen
   }
-//
+
   def circleTest() = {
     val (x, y) = (Random.nextFloat, Random.nextFloat)
     sqrt(x*x + y*y) <= 1
   }
-//
+
   def monteCarlo(trials: Int, test:() => Boolean) = Stream.continually(if (test()) 1.00 else 0.00)
     .take(trials)
     .sum / trials
